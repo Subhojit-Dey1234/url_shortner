734e95614f325a41d7b55ae57881a97338b9b124
Subhojit-Dey1234 <subhojit2000dey@gmail.com>
2021-09-03 12:42:08 +0530

    Added Header

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -23,7 +23,7 @@ export default class AppNavbar extends Component {
 			<div>
 				<Navbar color="dark" dark expand="sm" className="mb-5">
 					<Container>
-						<NavbarBrand href="/"> Shopping Cart</NavbarBrand>
+						<NavbarBrand href="/"> URL SHORTNER</NavbarBrand>
 						<NavbarToggler onClick={this.toggle} />
 						<Collapse isOpen={this.state.isOpen} navbar>
 							<Nav className="ml-auto" navbar>
